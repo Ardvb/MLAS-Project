bf9b9d61eded7691d635ebd5c4847f7414c550d1
unknown <ardvanbalkom@gmail.com>
2020-12-18 03:12:54 +0000

    Added javascript functions

--- GradBoosting.py
+++ GradBoosting.py
@@ -9,21 +9,21 @@ windpower = pd.read_csv('https://raw.githubusercontent.com/ianmcloughlin/2020A-m
 
 windpowernew = windpower.drop(windpower[(windpower["speed"]> 10) & (windpower["power"]==0)].index)
 
-class GradBoosting:
-    def gbrmodel(self, x):
-      
-      x = windpowernew.speed.values.reshape(-1,1) # Reshape to values between -1 and 1 so we can use regression analysis later
-      y = windpowernew["power"]
 
-      x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0) # Using random state to make test reproducible.
 
-      modelgbr = GradientBoostingRegressor(random_state=0).fit(x_train, y_train)
-      modelgbr.score(x_test, y_test)
 
-  # Make Prediction Using Wind Speed
-      prediction = modelgbr.predict([x])
 
-        # Stores elemtent of array as variable so can be returned
-      result = round(np.ndarray.item(prediction[0]),2)
+def gbrmodel():
+  x = windpowernew.speed.values.reshape(-1,1) # Reshape to values between -1 and 1 so we can use regression analysis later
+  y = windpowernew["power"]
 
-      return {"value": result}
+  x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0) # Using random state to make test reproducible.
+
+  modelgbr = GradientBoostingRegressor(random_state=0).fit(x_train, y_train)
+  modelgbr.score(x_test, y_test)
+  rdinp = np.array(np.random.uniform(low=1.0, high=24.0)) # Generate a random number between 1 and 24, representing the wind speed. Between those values our model works most accurately.
+  rdinprs = rdinp.reshape(-1,1) # Reshape the array to fit the model
+  prediction = modelgbr.predict(rdinprs)
+  return {"value": prediction}
+
+
--- server.py
+++ server.py
@@ -1,23 +1,39 @@
 import flask as fl
-from GradBoosting import GradBoosting as gb     
-from RandomForest import RandomForest as rf  
+import numpy as np
+#from GradBoosting import GradBoosting as gb     
+#from RandomForest import RandomForest as rf  
 
 
 # Create Web App
 app = fl.Flask(__name__, static_url_path = '', static_folder = 'staticpages')
 
+
 @app.route('/')
-def home():
+def index():
     return app.send_static_file('index.html')
 
-# Route Wind Speed Request to Sigmoid Neural Network Model
-@app.route('/api/gradboosting/<string:windpowernew>')
-def gradboostpred(windpowernew):
+@app.route('/api/uniform')
+def uniform():
+  return {"value": np.random.uniform(low=1.0, high=24.0)}
+
+@app.route('/api/grad')
+def grad():
+  return {"value": np.random.uniform(low=1.0, high=24.0)}
+
+@app.route('/api/randfor')
+def randfor():
+  return {"value": np.random.uniform(low=1.0, high=24.0)}
+
+#@app.route('/gradboost/<string:windpowernew>')
+#def gradboostpred(windpowernew):
     
     # String must be converted to float as cannot pass natively
     # Insight from https://github.com/pallets/flask/issues/315
-    windpowernew = float(windpowernew)
-    return gb.gbrmodel(windpowernew)
+    #windpowernew = float(windpowernew)
+    #return gb.gbrmodel(windpowernew)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
