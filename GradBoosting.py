07547fb17ce06aed52884a04cfc34a11a54ed567
unknown <ardvanbalkom@gmail.com>
2020-12-21 00:07:24 +0000

    html page working fine

--- GradBoosting.py
+++ GradBoosting.py
@@ -1,18 +1,22 @@
 # pylint: disable=no-member  
 # Above statement is just to disable an error message
 
-
+# To read in dataset
 import pandas as pd   
+
+# for numerical work
 import numpy as np
+
+# to use gradient booster model
 from sklearn.ensemble import GradientBoostingRegressor
+
+# To turn array into list, so html page can handle it
 import json
 
-# To split dataset into train and test parts
-# from sklearn.model_selection import train_test_split
 
 windpower = pd.read_csv('https://raw.githubusercontent.com/ianmcloughlin/2020A-machstat-project/master/dataset/powerproduction.csv')
 
-windpowernew = windpower.drop(windpower[(windpower["speed"]> 10) & (windpower["power"]==0)].index)
+windpowernew = windpower.drop(windpower[(windpower["speed"]> 10) & (windpower["power"]==0)].index) # Don't use speed values above 10 with a corresponding power output of 0
 
 
 class GradModel:
@@ -25,7 +29,7 @@ class GradModel:
       # Adapted from https://stackoverflow.com/questions/61246741/how-to-take-user-input-from-a-single-line-in-numpy-array
 
       windspeedrs = windspeed.reshape(-1,1) # Reshape the array to fit the model
-      power_output = modelgb.predict(np.array(windspeedrs))
+      power_output = modelgb.predict(np.array(windspeedrs)) # Predict power output using wind speed (output is an array)
       powerlist = power_output.tolist() # Turn array into list so it can be read into html file # Adapted from https://www.kite.com/python/answers/how-to-serialize-a-numpy-array-into-json-in-python
-      powerjson = json.dumps(powerlist)
-      return{"value": powerjson}
+      powerjson = json.dumps(*powerlist) # Add * to get rid of the brackets
+      return{"value": powerjson} # Return value to be outputted on html page
--- server.py
+++ server.py
@@ -6,25 +6,30 @@ from GradBoosting import GradModel
 
 # To be able to use Random Forest Model
 from RandomForest import RanFModel
+
+# To generate random number
 import numpy as np
  
 # Create Web App
 app = fl.Flask(__name__, static_url_path = '', static_folder = 'staticpages')
 
 
-
+# Route for home page
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
+# Route for predicting power-output using Gradient Boosting model
 @app.route('/api/gradient/<string:inpspeed>') 
 def grboosting(inpspeed):
   return GradModel.gbmodel(inpspeed)
 
+# Route for predicting power-output using Random Forest Model
 @app.route('/api/randfor/<string:inpspeed>') 
 def randforest(inpspeed):
   return RanFModel.rfmodel(inpspeed)
 
+# Route to generate random number between 1 and 24
 @app.route('/api/random') 
 def random():
   return {"value": round(np.random.uniform(low=1.0, high=24.0),2)}
